6ca7111e5a0f90044ad22dd6f2d45ab7cb8c2d9f
Sue Le Marinel <sue.lemarinel@gmail.com>
2025-11-22 13:22:24 +0100

    Routes/hashtag - Post new hashtag, get all hashtag, get specific hashtag

---
+++ routes/hashtag.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+require('../models/connection');
+const Hashtag = require('../models/hashtag');
+
+// POST new hashtag
+router.post('/', (req, res) => {
+    if (!req.body.tag) {
+        res.json({ result: false, error: "Missing tag" });
+        return;
+    }
+
+    const newTag = new Hashtag({
+        tag: req.body.tag,
+        count: req.body.count || 1
+    });
+
+    newTag.save()
+        .then(doc => res.json({ result: true, hashtag: doc }))
+        .catch(err => res.json({ result: false, error: err.message }));
+});
+
+// GET all hashtags
+router.get('/', (req, res) => {
+    Hashtag.find({})
+        .then(tags => res.json({ result: true, hashtags: tags }))
+        .catch(err => res.json({ result: false, error: err.message }));
+});
+
+// GET specific hashtag
+router.get('/:tag', (req, res) => {
+    Hashtag.findOne({ tag: req.params.tag })
+        .then(tag => res.json({ result: true, hashtag: tag }))
+        .catch(err => res.json({ result: false, error: err.message }));
+});
+
+module.exports = router;
